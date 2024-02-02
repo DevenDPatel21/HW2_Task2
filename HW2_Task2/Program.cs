8225d525c61cdbcd2d6116688b44a65716523e32
Deven Patel <Deven.D.Patel-1@ou.edu>
2024-02-02 14:51:12 -0600

    Add project files.

---
+++ HW2_Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// MIS 3033
+// HW 2 Task 2
+// Deven Patel
+// 113566222
+
+
+//Dictionary<key, value> dict_name;
+// dict_name = new Dictrion
+
+Dictionary<string, double> fruitDict= new Dictionary<string, double>();
+
+fruitDict.Add("apples",0.99);
+fruitDict.Add("oranges",0.5);
+fruitDict.Add("bananas",0.5);
+fruitDict.Add("grapes",2.99);
+fruitDict.Add("blueberries",1.99);
+
+Console.WriteLine("Fruit names: ");
+
+
+
+
+for (int i = 0; i < fruitDict.Count; i++)
+{
+    Console.Write(fruitDict.Keys.ElementAt(i) + "   ");
+}
+
+Console.WriteLine("\n");
+Console.Write("input the fruit name: ");
+string userInputStr = Console.ReadLine();
+
+if (fruitDict.ContainsKey(userInputStr))
+{
+
+    string outMesstr = string.Format($"The price of {userInputStr} is {fruitDict[userInputStr]}:C2");
+    Console.WriteLine(outMesstr);
+}
+else
+{
+    Console.WriteLine("Sorry! Not found! Wrong fruit name !");
+}
+Console.ReadLine();
